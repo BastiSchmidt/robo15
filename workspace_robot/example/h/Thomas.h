ccc5f3cd837f6c0b68949ae50599e9779356d9ae
Zilgan <Thomas.Adlmaier@gmx.de>
2015-03-18 11:32:18 +0100

    Gemeinsame Datei
    Gemeinsame Datei

---
+++ workspace_robot/example/h/Thomas.h
@@ -0,0 +1,32 @@
+/*
+ * Thomas.h
+ *
+ *  Created on: 17.03.2015
+ *      Author: Thomas
+ */
+
+#ifndef H_THOMAS_H_
+#define H_THOMAS_H_
+
+#include <stdlib.h>
+#include "kernel.h"
+/* Will be generated: */
+#include "kernel_id.h"
+#include "ecrobot_interface.h"
+#include "../src/Thomas.c"
+
+
+void wait_ms(int ms);
+void drehen_grad_r(int grad);
+void drehen_grad_l(int grad);
+void drehen();
+void scanNode(int orientation);
+void drive(int umdr);
+void step_left(int time);
+void step_right(int time);
+int steps_left(int steps, int steplenght);
+void checkline();
+
+
+
+#endif /* H_THOMAS_H_ */
---
+++ workspace_robot/example/h/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdlib.h>
+#include "kernel.h"
+/* Will be generated: */
+#include "kernel_id.h"
+#include "ecrobot_interface.h"
+/* add user headers here: */
+#include "Thomas.h"
+/* void hello_world(); */
+
+#endif /* MAIN_H_ */
