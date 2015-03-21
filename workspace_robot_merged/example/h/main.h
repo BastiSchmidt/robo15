54002f651158b8cd3f882fa9873fe9bc0133962d
toothstone <toothstone@wh2.tu-dresden.de>
2015-03-21 18:24:48 +0100

    Add dirty merge workspace

---
+++ workspace_robot_merged/example/h/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdlib.h>
+#include "kernel.h"
+/* Will be generated: */
+#include "kernel_id.h"
+#include "ecrobot_interface.h"
+/* add user headers here: */
+#include "Thomas.h"
+//#include "../src/Thomas.c"
+int SCHWARZ;
+/* void hello_world(); */
+
+#endif /* MAIN_H_ */
